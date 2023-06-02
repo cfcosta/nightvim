be753c3fdb18611c3a0750daa387758f206030c9
Cainã Costa <me@cfcosta.com>
2023-06-02 18:32:47 -0300

    feat: gitignore and docs for mapped options

--- flake.nix
+++ flake.nix
@@ -21,22 +21,52 @@
           options = {
             tabstop = mkOption {
               type = lib.types.int;
-              default = 4;
+              description = ''
+                Number of spaces that a <Tab> in the file counts for.  Also see
+                the |:retab| command, and the 'softtabstop' option.
+              '';
+              default = 8;
             };
 
             softtabstop = mkOption {
               type = lib.types.int;
-              default = 4;
+              description = ''
+                Number of spaces that a <Tab> counts for while performing editing
+                operations, like inserting a <Tab> or using <BS>.  It "feels" like
+                <Tab>s are being inserted, while in fact a mix of spaces and <Tab>s is
+                used.  This is useful to keep the 'ts' setting at its standard value
+                of 8, while being able to edit like it is set to 'sts'.
+              '';
+              default = 0;
             };
 
             shiftwidth = mkOption {
               type = lib.types.int;
-              default = 4;
+              description = ''
+                Number of spaces to use for each step of (auto)indent.  Used for
+                |'cindent'|, |>>|, |<<|, etc.
+              '';
+              default = 8;
             };
 
-            expandtab = mkEnableOption "expandtab";
-            number = mkEnableOption "number";
-            relativenumber = mkEnableOption "relativenumber";
+            expandtab = mkEnableOption ''
+              In Insert mode: Use the appropriate number of spaces to insert a
+              <Tab>. Spaces are used in indents with the '>' and '<' commands and
+              when 'autoindent' is on.
+            '';
+
+            number = mkEnableOption ''
+              Print the line number in front of each line.  When the 'n' option is
+              excluded from 'cpoptions' a wrapped line will not use the column of
+              line numbers.
+            '';
+
+            relativenumber = mkEnableOption ''
+              Show the line number relative to the line with the cursor in front of
+              each line. Relative line numbers help you use the |count| you can
+              precede some vertical motion commands (e.g. j k + -) with, without
+              having to calculate it yourself.
+            '';
           };
 
           globals = {
