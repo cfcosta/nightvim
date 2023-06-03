3bf8801ea3b006a5c4ad67c69c8e46a0f6da9417
Cainã Costa <me@cfcosta.com>
2023-06-03 12:08:22 -0300

    chore: remove some features
    The first thing to get right is the plugin management side. We can add
    the other parts later, but this is the part that should matter more.

--- example/flake.nix
+++ example/flake.nix
@@ -10,169 +10,167 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nightvim.url = "github:cfcosta/nightvim";
+    nightvim = {
+      url = "github:cfcosta/nightvim";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
 
-    cmp-buffer.url = "github:hrsh7th/cmp-buffer";
-    cmp-cmdline.url = "github:hrsh7th/cmp-cmdline";
-    cmp-nvim-lsp.url = "github:hrsh7th/cmp-nvim-lsp";
-    cmp-path.url = "github:hrsh7th/cmp-path";
-    cmp-snippy.url = "github:dcampos/cmp-snippy";
-    codegpt.url = "github:dpayne/codegpt.nvim";
-    comment.url = "github:numtostr/comment.nvim";
-    diffview.url = "github:sindrets/diffview.nvim";
-    gitsigns.url = "github:lewis6991/gitsigns.nvim";
-    mason-lspconfig.url = "github:williamboman/mason-lspconfig.nvim";
-    mason.url = "github:williamboman/mason.nvim";
-    neo-tree.url = "github:nvim-neo-tree/neo-tree.nvim";
-    neogit.url = "github:TimUntersberger/neogit";
-    neorg.url = "github:nvim-neorg/neorg";
-    nui.url = "github:muniftanjim/nui.nvim";
-    null-ls.url = "github:jose-elias-alvarez/null-ls.nvim";
-    nvim-autopairs.url = "github:windwp/nvim-autopairs";
-    nvim-cmp.url = "github:hrsh7th/nvim-cmp";
-    nvim-dap.url = "github:mfussenegger/nvim-dap";
-    nvim-lspconfig.url = "github:neovim/nvim-lspconfig";
-    nvim-snippy.url = "github:dcampos/nvim-snippy";
-    nvim-surround.url = "github:kylechui/nvim-surround";
-    nvim-treesitter-endwise.url = "github:RRethy/nvim-treesitter-endwise";
-    nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter";
-    plenary.url = "github:nvim-lua/plenary.nvim";
-    rust-tools.url = "github:simrat39/rust-tools.nvim";
-    telescope.url = "github:nvim-telescope/telescope.nvim";
-    toggleterm.url = "github:akinsho/toggleterm.nvim";
-    tokyonight.url = "github:folke/tokyonight.nvim";
-    trouble.url = "github:folke/trouble.nvim";
+    cmp-buffer = {
+      url = "github:hrsh7th/cmp-buffer";
+      flake = false;
+    };
+    cmp-cmdline = {
+      url = "github:hrsh7th/cmp-cmdline";
+      flake = false;
+    };
+    cmp-nvim-lsp = {
+      url = "github:hrsh7th/cmp-nvim-lsp";
+      flake = false;
+    };
+    cmp-path = {
+      url = "github:hrsh7th/cmp-path";
+      flake = false;
+    };
+    cmp-snippy = {
+      url = "github:dcampos/cmp-snippy";
+      flake = false;
+    };
+    codegpt = {
+      url = "github:dpayne/codegpt.nvim";
+      flake = false;
+    };
+    comment = {
+      url = "github:numtostr/comment.nvim";
+      flake = false;
+    };
+    diffview = {
+      url = "github:sindrets/diffview.nvim";
+      flake = false;
+    };
+    gitsigns = {
+      url = "github:lewis6991/gitsigns.nvim";
+      flake = false;
+    };
+    neo-tree = {
+      url = "github:nvim-neo-tree/neo-tree.nvim";
+      flake = false;
+    };
+    neogit = {
+      url = "github:TimUntersberger/neogit";
+      flake = false;
+    };
+    neorg = {
+      url = "github:nvim-neorg/neorg";
+      flake = false;
+    };
+    nui = {
+      url = "github:muniftanjim/nui.nvim";
+      flake = false;
+    };
+    null-ls = {
+      url = "github:jose-elias-alvarez/null-ls.nvim";
+      flake = false;
+    };
+    nvim-autopairs = {
+      url = "github:windwp/nvim-autopairs";
+      flake = false;
+    };
+    nvim-cmp = {
+      url = "github:hrsh7th/nvim-cmp";
+      flake = false;
+    };
+    nvim-dap = {
+      url = "github:mfussenegger/nvim-dap";
+      flake = false;
+    };
+    nvim-lspconfig = {
+      url = "github:neovim/nvim-lspconfig";
+      flake = false;
+    };
+    nvim-snippy = {
+      url = "github:dcampos/nvim-snippy";
+      flake = false;
+    };
+    nvim-surround = {
+      url = "github:kylechui/nvim-surround";
+      flake = false;
+    };
+    nvim-treesitter-endwise = {
+      url = "github:RRethy/nvim-treesitter-endwise";
+      flake = false;
+    };
+    nvim-treesitter = {
+      url = "github:nvim-treesitter/nvim-treesitter";
+      flake = false;
+    };
+    plenary = {
+      url = "github:nvim-lua/plenary.nvim";
+      flake = false;
+    };
+    rust-tools = {
+      url = "github:simrat39/rust-tools.nvim";
+      flake = false;
+    };
+    telescope = {
+      url = "github:nvim-telescope/telescope.nvim";
+      flake = false;
+    };
+    toggleterm = {
+      url = "github:akinsho/toggleterm.nvim";
+      flake = false;
+    };
+    tokyonight = {
+      url = "github:folke/tokyonight.nvim";
+      flake = false;
+    };
+    trouble = {
+      url = "github:folke/trouble.nvim";
+      flake = false;
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, home-manager, nightvim, ... }:
     let
-      system = "x86_64-linux";
+      system = "aarch64-darwin";
       pkgs = import nixpkgs { inherit system; };
+      vimConfig = { options, config, lib, pkgs }: {
+        imports = [ nightvim.hmModule ];
+        programs.nightvim = {
+          plugins = with inputs; [
+            cmp-buffer
+            cmp-cmdline
+            cmp-nvim-lsp
+            cmp-path
+            cmp-snippy
+            codegpt
+            comment
+            diffview
+            gitsigns
+            neo-tree
+            neogit
+            neorg
+            nui
+            null-ls
+            nvim-autopairs
+            nvim-cmp
+            nvim-dap
+            nvim-lspconfig
+            nvim-snippy
+            nvim-surround
+            nvim-treesitter-endwise
+            nvim-treesitter
+            plenary
+            rust-tools
+          ];
+        };
+      };
     in {
-      home = home-manager.lib.homeManagerConfiguration {
+      result = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        modules = [
-          nightvim.hmModule
-          ({ options, config, lib, pkgs }: {
-            programs.nightvim = {
-              enable = true;
-
-              options = {
-                tabstop = 2;
-                softtabstop = 2;
-                shiftwidth = 2;
-                expandtab = true;
-                number = true;
-                relativenumber = true;
-              };
-
-              globals.mapleader = " ";
-
-              keybindings = {
-                n."<leader>wv" = "<cmd>vsplit<cr>";
-                n."<leader>ws" = "<cmd>split<cr>";
-                n."<leader>wc" = "<cmd>close<cr>";
-                n."<leader>wo" = "<cmd>only<cr>";
-              };
-
-              plugins = with inputs; [
-                ({
-                  source = tokyonight;
-                  lazy = false;
-                  config = ''
-                    vim.cmd [[colorscheme tokyonight]]
-                  '';
-                })
-                ({
-                  source = codegpt;
-                  dependencies = [ nui plenary ];
-                  commands = [ "Chat" ];
-                  config = ''
-                    if os.getenv "OPENAI_API_KEY" then
-                      require "codegpt.config"
-                    end
-                  '';
-                })
-                ({ source = comment; })
-                ({ source = gitsigns; })
-                ({
-                  source = mason;
-                  dependencies = [
-                    nvim-dap
-                    nvim-lspconfig
-                    plenary
-                    rust-tools
-                    mason-lspconfig
-                    nvim-lspconfig
-                    cmp-nvim-lsp
-                    cmp-buffer
-                    cmp-path
-                    cmp-cmdline
-                    nvim-cmp
-                    nvim-snippy
-                    cmp-snippy
-                    neorg
-                  ];
-
-                  config = ''
-                    local lspconfig = require "lspconfig"
-                    local lsp_defaults = lspconfig.util.default_config
-
-                    lsp_defaults.capabilities =
-                      vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-                    require("mason").setup {
-                      -- Adds the path from mason into the end of the PATH env variable.
-                      -- Since we are using Nix to install it, we can assume that it is
-                      -- going to come from somewhere else.
-                      PATH = "append",
-                    }
-                    require("mason-lspconfig").setup()
-                    require("mason-lspconfig").setup_handlers {
-                      function(server_name) -- default handler (optional)
-                        lspconfig[server_name].setup {}
-                      end,
-                      ["rust_analyzer"] = function()
-                        require("rust-tools").setup {
-                          capabilities = lsp_defaults.capabilities,
-                        }
-                      end,
-                    }
-
-                    local cmp = require "cmp"
-
-                    cmp.setup {
-                      snippet = {
-                        expand = function(args)
-                          require("snippy").expand_snippet(args.body)
-                        end,
-                      },
-                      window = {
-                        completion = cmp.config.window.bordered(),
-                        documentation = cmp.config.window.bordered(),
-                      },
-                      mapping = cmp.mapping.preset.insert {
-                        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                        ["<C-Space>"] = cmp.mapping.complete(),
-                        ["<C-e>"] = cmp.mapping.abort(),
-                        ["<CR>"] = cmp.mapping.confirm { select = true },
-                      },
-                      sources = cmp.config.sources({
-                        { name = "nvim_lsp" },
-                        { name = "snippy" },
-                        { name = "neorg" },
-                      }, {
-                        { name = "buffer" },
-                      }),
-                    }
-                  '';
-                })
-              ];
-            };
-          })
-        ];
+        modules = [ vimConfig ];
       };
     };
 }
