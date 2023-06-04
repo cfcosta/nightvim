1bca56ad25187287952cfa76824f4f2f6d4c9a48
Cainã Costa <me@cfcosta.com>
2023-06-04 12:50:38 -0300

    feat: fail example script if build fails, update input before running

--- example/test.sh
+++ example/test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "${ROOT}" || exit 1
 
+nix flake lock --update-input nightvim
 nix build
 
 XDG_CONFIG_HOME="${ROOT}/result/home-files/.config" nvim
