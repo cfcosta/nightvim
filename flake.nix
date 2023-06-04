659f1ab47c8b5ad6387dee0e8655af089c92b30b
Cainã Costa <me@cfcosta.com>
2023-06-03 21:35:21 -0300

    chore: add devshell

--- flake.nix
+++ flake.nix
@@ -11,43 +11,39 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager }: {
-    lib.mkPlugin = name: dir: spec:
-      spec // {
-        inherit name dir;
-        depends = nixpkgs.lib.mkDefault [ ];
-        inputs = nixpkgs.lib.mkDefault [ ];
-        config = nixpkgs.lib.mkDefault ''require("${name}").setup {}'';
-        keys.n = nixpkgs.lib.mkDefault { };
-      };
-    hmModule = { options, config, lib, pkgs, ... }:
-      let cfg = config.programs.nightvim;
-      in
-      {
-        options.programs.nightvim = with lib; {
-          enable = mkEnableOption "NightVim";
-
-          plugins = mkOption {
-            type = types.listOf types.attrs;
-            default = [ ];
-          };
-
-          extraConfig = mkOption {
-            type = types.str;
-            default = "";
-          };
+  outputs = { self, nixpkgs, flake-utils, home-manager }:
+    {
+      lib.mkPlugin = name: dir: spec:
+        spec // {
+          inherit name dir;
+          depends = nixpkgs.lib.mkDefault [ ];
+          inputs = nixpkgs.lib.mkDefault [ ];
+          config = nixpkgs.lib.mkDefault ''require("${name}").setup {}'';
+          keys.n = nixpkgs.lib.mkDefault { };
         };
+      hmModule = { options, config, lib, pkgs, ... }:
+        let cfg = config.programs.nightvim;
+        in {
+          options.programs.nightvim = with lib; {
+            enable = mkEnableOption "NightVim";
 
-        config =
-          let
+            plugins = mkOption {
+              type = types.listOf types.attrs;
+              default = [ ];
+            };
+
+            extraConfig = mkOption {
+              type = types.str;
+              default = "";
+            };
+          };
+
+          config = let
             pluginNames = builtins.map (p: p.name) cfg.plugins;
-            pluginFolders = lib.foldl'
-              (acc: attr:
-                acc // {
-                  "nvim/night/plugins/start/${attr.name}".source = attr.dir;
-                })
-              { }
-              cfg.plugins;
+            pluginFolders = lib.foldl' (acc: attr:
+              acc // {
+                "nvim/night/plugins/start/${attr.name}".source = attr.dir;
+              }) { } cfg.plugins;
             mapSpec = p: ''
               NightVim.setup_plugin(
                 "${p.name}",
@@ -56,8 +52,7 @@
                   ${builtins.toJSON p.config}
                 end
               )'';
-          in
-          (lib.mkIf cfg.enable {
+          in (lib.mkIf cfg.enable {
             programs.neovim.enable = true;
 
             xdg.configFile = pluginFolders // {
@@ -72,6 +67,17 @@
                 ${cfg.extraConfig}'';
             };
           });
-      };
-  };
+        };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            lua-language-server
+            stylua
+            nixfmt
+            rnix-lsp
+          ];
+        };
+      });
 }
